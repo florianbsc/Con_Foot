1e140aef2c2f62e56f7f1088c1f40dd4d950386b
florianbsc <floflo_200068@hotmail.fr>
2024-02-20 14:14:48 +0100

    first commit

---
+++ src/class/Humain.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\class;
+
+use DateTime;
+use mysql_xdevapi\Exception;
+
+class Humain
+{
+    //Liste des propriétés
+    private string $nom;
+    private string $prenom;
+    private DateTime $dateNaissance;
+
+    //Constructeur => Sert à créer l’objet
+    public function __construct(string $nom, string $prenom, DateTime $dateNaissance)
+    {
+        //On initialise les propriétés
+        //$this signifie “cette instance”, donc l'objet en cours
+        $this->nom = $nom; //$this->nom est la propriété nom de l'objet en cours
+                           //$nom est le paramètre de la fonction
+        $this->prenom = $prenom;
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    //Liste des méthodes
+    // Comment modifier des propriétés privées ?
+    // => On crée des méthodes qui s'appelle des accesseurs (getters) et des mutateurs (setters)
+    // Les assesseurs permettent de lire les propriétés privées
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function getDateNaissance(): DateTime
+    {
+        return $this->dateNaissance;
+    }
+
+    //Les mutateurs permettent de modifier les propriétés privées
+    public function setNom(string $nom): void
+    {
+        if($nom != "")
+            $this->nom = $nom;
+        else
+            throw new Exception(message: "le nom ne peut pas etre vide");
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function setDateNaissance(DateTime $dateNaissance): void
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    // => On peut aussi créer des méthodes qui ne modifient pas les propriétés mais qui les utilisent
+    public function donneTexte(): string
+    {
+        return "Humain " . $this->nom . " " . $this->prenom . " date de naissance " . $this->dateNaissance->format('d/m/Y');
+    }
+}
